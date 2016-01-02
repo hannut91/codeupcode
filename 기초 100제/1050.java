4525a23ac8f9df066ab24c9e9668035f5e8ac201
hannut91 <hannut91@gmail.com>
2016-01-02 11:54:15 +0900

    1/2 1049~1053

---
+++ 기초 100제/1050.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = num1 == num2 ? 1 : 0;
+		System.out.print(num3);
+	}
+
+}
